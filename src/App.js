963fb12c194be8e5b90cf39c334e23a2b1ba0ff0
Matt Anglin <mattanglin@gmail.com>
2021-08-23 16:11:52 -0700

    Pitfalls of local state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Counter } from './Counter';
 import './App.css';
 
+const pages = [
+  'counter',
+  'page2',
+  'page3',
+  'page4',
+];
+
 function App() {
+  const [currentPage, setCurrentPage] = useState('counter');
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>State Management with Redux</h1>
-        <Counter />
+        <div>page: /{currentPage}</div>
+        <ul className="nav">
+          {pages.map((pageName) => (
+            <li key={pageName}>
+              <button onClick={() => setCurrentPage(pageName)}>{pageName}</button>
+            </li>
+          ))}
+        </ul>
+        <div>
+          {currentPage === 'counter' ? (
+            <Counter />
+          ) : (
+            <div>
+              Now we're on {currentPage}
+            </div>
+          )}
+        </div>
       </header>
     </div>
   );
